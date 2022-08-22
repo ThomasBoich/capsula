569529d535126b7d859b31d98e7426bd1afe4d1d
unknown <boyko.a.v@bk.ru>
2022-08-23 01:06:29 +0700

    рабочие категории

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import get_category, news
-from .models import *
 
 
 urlpatterns = [
     path('', news),
-    path('category/<int:list_category>/', get_category),
+    path('category/<int:category_id>/', get_category),
 ]
--- news/views.py
+++ news/views.py
@@ -4,19 +4,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News, Category
 
-
 def news(request):
     # return HttpResponse('Страница Новостей')
-    list_news = News.objects.order_by('-created_att')
-    categories = Category.objects.all()
-    content = {
-        'news': list_news,
+    # list_news = News.objects.order_by('-created_att')
+    # categories = Category.objects.all()
+    # content = {
+    #     'news': list_news,
+    #     'title': 'Список новостей',
+    #     'category': categories,
+    # }
+    # return render(request, template_name='news/index.html', context=content)
+    news = News.objects.all()
+    category = Category.objects.all()
+    context = {
+        'news': news,
         'title': 'Список новостей',
-        'category': categories,
-    }
-    return render(request, template_name='news/index.html', context=content)
+        'categories': category,
+       }
+    return render(request, template_name='news/index.html', context=context)
 
-def get_category(request, get_category_id):
-
-    category = get_category_id
-    return render(request, template_name='news/category.html',)
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.all()
+    category_key = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': category, 'category_key': category_key, 'category': category,})
